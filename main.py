2afd273a9ca2ff87fdd18e57392f221dbcdadb4c
samir-bueno <samirbueno585@gmail.com>
2025-03-27 13:33:16 -0300

    actualize el main para poder agregar productos y pedidos(con cliente_id y producto_id)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from db.modelos import Clientes, Productos
 from app.clientes.registrar import registrar as registrarNuevoCliente
+from app.productos.registrar import registrar as registrarNuevoProducto
+from app.pedidos.registrar import PedidoARegistrar, registrar as registrarNuevoPedido
 from app import app 
 
 @app.get("/api/saludo")
@@ -9,3 +11,11 @@ def read_root():
 @app.post("/api/v1/clientes")
 def post_clientes(cliente: Clientes):
     return registrarNuevoCliente(cliente)
+
+@app.post("/api/v1/productos")
+def post_productos(producto: Productos):
+    return registrarNuevoProducto(producto)
+
+@app.post("/api/v1/pedidos")
+def post_pedidos(pedido: PedidoARegistrar):
+    return registrarNuevoPedido(pedido)
