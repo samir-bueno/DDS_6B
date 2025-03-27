a334912e81ef8ee1d3ba59e023bbbb1502ad9dd5
samir-bueno <samirbueno585@gmail.com>
2025-03-27 14:03:33 -0300

    ahora los clientes pueden ver el estado de un pedido especifico

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from db.modelos import Clientes, Productos
 from app.clientes.registrar import registrar as registrarNuevoCliente
 from app.productos.registrar import registrar as registrarNuevoProducto
 from app.pedidos.registrar import PedidoARegistrar, registrar as registrarNuevoPedido
+from app.clientes.consultar_pedido import consultarPedidos
+from app.clientes.consultar_pedido_especifico import consultarPedidoEspecifico
 from app import app 
 
 @app.get("/api/saludo")
@@ -19,3 +21,13 @@ def post_productos(producto: Productos):
 @app.post("/api/v1/pedidos")
 def post_pedidos(pedido: PedidoARegistrar):
     return registrarNuevoPedido(pedido)
+
+@app.get("/api/v1/clientes/{cliente_id}/pedidos")
+def get_pedidos(cliente_id: str):
+    pedidos = consultarPedidos(cliente_id)
+    return {"pedidos": pedidos}
+
+@app.get("/api/v1/clientes/{cliente_id}/pedidos/{pedido_id}/estado")
+def get_pedidos_especifico(cliente_id: str, pedido_id: str):
+    pedido = consultarPedidoEspecifico(cliente_id, pedido_id)
+    return {"id": pedido.id, "pedidos": pedido.estado}
