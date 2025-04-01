f63cd09376e524fb246839b61833007649577f81
samir-bueno <samirbueno585@gmail.com>
2025-04-01 12:35:57 -0300

    cree la estructura principal tal cual decia el tutorial para empezar con la creacion del bot

---
+++ bots_con_discord/bot.py
@@ -0,0 +1,26 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+# Cargar variables de entorno
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# Crear cliente
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'✅ Bot conectado como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return  
+
+    if message.content.lower() == 'hola':
+        await message.channel.send('¡Hola! Soy un bot hecho en Python. 🤖')
+
+client.run(TOKEN)
